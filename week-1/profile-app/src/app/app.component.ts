78a8a2bd057ac59d2cc4a605a10a0584efd837c5
CSSkelton <codysskelton@gmail.com>
2024-03-24 21:53:24 -0400

    code attribution

--- week-1/profile-app/src/app/app.component.ts
+++ week-1/profile-app/src/app/app.component.ts
@@ -1,3 +1,11 @@
+/**
+*Title: app.component.ts
+*Purpose: main component TypeScript file
+*Date: 03.24.2024
+*Author: Cody Skelton
+*Code attribution: Code sourced from Professor Krasso's YouTube video at https://www.youtube.com/watch?v=XRm0SnUJROU&ab_channel=Devression and WEB 425's Exercise 1.5 program requirements
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
