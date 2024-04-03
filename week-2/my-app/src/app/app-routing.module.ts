042ea6ce74d79d7c6193d02319b94ad9600a22d4
CSSkelton <codysskelton@gmail.com>
2024-04-02 23:00:09 -0400

    week 2 exercise 2 - navigation

---
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+// go to home component
+// http://localhost:4200
+// http://localhost:4200/home
+
+// go to contact component
+// http://localhost:4200/contact
+
+// go to about component
+// http://localhost:4200/about
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
